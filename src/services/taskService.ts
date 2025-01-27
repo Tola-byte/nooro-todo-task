476ca64ca51ae0d458a69dd8668edae942a64cb0
Tola-byte <adetolaisaiah17@gmail.com>
2025-01-27 12:39:42 +0100

    write proper url

--- src/services/taskService.ts
+++ src/services/taskService.ts
@@ -2,9 +2,12 @@ import { Task } from '@/types/TaskList';
 import axios from 'axios';
 
 
+const BASE_API_URL = process.env.NEXT_PUBLIC_API_BASE_URL
+
 export const fetchTasks = async () => {
-  try {
-    const response = await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/getTasks`);
+    try {
+   
+    const response = await axios.get(`${BASE_API_URL}/getTasks`);
     return response.data;
   } catch (error) {
     console.error('Error fetching tasks:', error);
@@ -14,7 +17,7 @@ export const fetchTasks = async () => {
 
 export const addTask = async (task: { title: string; color: string }) => {
   try {
-    const response = await axios.post(`${process.env.NEXT_PUBLIC_API_BASE_URL}/postTasks`, task);
+    const response = await axios.post(`${BASE_API_URL}/postTasks`, task);
     return response.data;
   } catch (error) {
     console.error('Error adding task:', error);
@@ -25,7 +28,7 @@ export const addTask = async (task: { title: string; color: string }) => {
 export const editTask = async (updates: Task) => {
   try {
     const response = await axios.put(
-      `${process.env.NEXT_PUBLIC_API_BASE_URL}/editTasks/${updates.id}`,
+      `${BASE_API_URL}/editTasks/${updates.id}`,
       updates
     );
     return response.data;
@@ -37,7 +40,7 @@ export const editTask = async (updates: Task) => {
 
 export const deleteTask = async (id: number) => {
   try {
-    const response = await axios.delete(`${process.env.NEXT_PUBLIC_API_BASE_URL}/deleteTasks/${id}`);
+    const response = await axios.delete(`${BASE_API_URL}/deleteTasks/${id}`);
     return response.data;
   } catch (error) {
     console.error('Error deleting task:', error);
